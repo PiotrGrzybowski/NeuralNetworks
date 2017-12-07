4fe4295761bc1a1df5d52e69ce574256626833c6
Piotr <p.grzybowski2@gmail.com>
2017-12-07 23:56:26 +0100

    lelo

--- networks/autoencoder_tf/encoder.py
+++ networks/autoencoder_tf/encoder.py
@@ -8,33 +8,30 @@ class Network:
     def __init__(self, hidden):
         self.input = tf.placeholder(tf.float32, shape=[784, None], name="input")
         self.loss_function = MeanSquaredError()
+        
+        self.weights = [tf.Variable(tf.random_normal(shape=[hidden, 784], stddev=0.35), name="hidden_weights"),
+                        tf.Variable(tf.random_normal(shape=[784, hidden], stddev=0.35), name="output_weights")]
+        
+        self.biases = [tf.Variable(tf.ones(shape=[hidden, 1], name="hidden_bias")),
+                       tf.Variable(tf.ones(shape=[784, 1], name="output_bias"))]
+        
+        self.nets = [tf.matmul(self.weights[0], self.input),
+                     tf.matmul(self.weights[1], self.activations[0])]
+        self.activations = [tf.sigmoid(self.nets[0]),
+                            tf.sigmoid(self.nets[1])]
+        self.activation_derivatives = [sigmoid_derivative(self.nets[0]), sigmoid_derivative(self.nets[1])]
 
-        self.hidden_weights = tf.Variable(tf.random_normal(shape=[hidden, 784], stddev=0.35), name="hidden_weights")
-        self.output_weights = tf.Variable(tf.random_normal(shape=[784, hidden], stddev=0.35), name="output_weights")
-
-        self.hidden_bias = tf.Variable(tf.ones(shape=[hidden, 1], name="hidden_bias"))
-        self.output_bias = tf.Variable(tf.ones(shape=[784, 1], name="output_bias"))
-
-        self.hidden_net = tf.matmul(self.hidden_weights, self.input)
-        self.hidden_activation = tf.sigmoid(self.hidden_net)
-        self.hidden_activation_derivative = sigmoid_derivative(self.hidden_net)
-
-        self.output_net = tf.matmul(self.output_weights, self.hidden_activation)
-        self.output_activation = tf.sigmoid(self.output_net)
-        self.output_activation_derivative = sigmoid_derivative(self.output_net)
-
-        self.output_error = tf.multiply(self.loss_function.calculate_cost_gradient(self.input, self.output_activation),
-                                        self.output_activation_derivative)
-
-        self.hidden_error = tf.multiply(tf.matmul(tf.transpose(self.output_weights), self.output_error),
-                                        self.hidden_activation_derivative)
-
-        self.output_weights_gradient = tf.matmul(self.output_error, tf.transpose(self.hidden_activation))
-        self.hidden_weights_gradient = tf.matmul(self.hidden_error, tf.transpose(self.input))
-
-        self.output_bias_gradient = tf.expand_dims(tf.reduce_sum(self.output_error, axis=1), axis=1)
-        self.hidden_bias_gradient = tf.expand_dims(tf.reduce_sum(self.hidden_error, axis=1), axis=1)
-
+        self.errors = [tf.multiply(self.loss_function.calculate_cost_gradient(self.input, self.activations[1]),
+                                   self.activation_derivatives[1]),
+                       tf.multiply(tf.matmul(tf.transpose(self.weights[1]), self.errors[1]),
+                                   self.activation_derivatives[0])]
+        
+        self.weights_gradient = [tf.matmul(self.errors[0], tf.transpose(self.input)),
+                                 tf.matmul(self.errors[1], tf.transpose(self.activations[0]))]
+        
+        self.bias_gradient = [tf.expand_dims(tf.reduce_sum(self.errors[0], axis=1), axis=1),
+                              tf.expand_dims(tf.reduce_sum(self.errors[1], axis=1), axis=1)]
+        
     def train(self, training_data, eta, epochs, batch_size, l2, test_data=None):
         init = tf.global_variables_initializer()
         with tf.Session() as sess:
@@ -47,19 +44,19 @@ class Network:
                     print(k)
                     k += 1
                     steps = [
-                        tf.assign(self.output_weights, tf.subtract(
-                            tf.multiply(tf.subtract(tf.constant(1.0), tf.multiply(eta, l2)), self.output_weights),
-                            tf.multiply(tf.div(eta, batch_size), self.output_weights_gradient))),
+                        tf.assign(self.weights[1], tf.subtract(
+                            tf.multiply(tf.subtract(tf.constant(1.0), tf.multiply(eta, l2)), self.weights[1]),
+                            tf.multiply(tf.div(eta, batch_size), self.weights_gradient[1]))),
 
-                        tf.assign(self.hidden_weights, tf.subtract(
-                            tf.multiply(tf.subtract(tf.constant(1.0), tf.multiply(eta, l2)), self.hidden_weights),
-                            tf.multiply(tf.div(eta, batch_size), self.hidden_weights_gradient))),
+                        tf.assign(self.weights[0], tf.subtract(
+                            tf.multiply(tf.subtract(tf.constant(1.0), tf.multiply(eta, l2)), self.weights[0]),
+                            tf.multiply(tf.div(eta, batch_size), self.weights_gradient[0]))),
 
-                        tf.assign(self.output_bias, tf.subtract(self.output_bias, tf.multiply(tf.div(eta, batch_size),
-                                                                                              self.output_bias_gradient))),
+                        tf.assign(self.biases[1], tf.subtract(self.biases[1], tf.multiply(tf.div(eta, batch_size),
+                                                                                              self.bias_gradient[1]))),
 
-                        tf.assign(self.hidden_bias, tf.subtract(self.hidden_bias, tf.multiply(tf.div(eta, batch_size),
-                                                                                              self.hidden_bias_gradient)))]
+                        tf.assign(self.biases[0], tf.subtract(self.biases[0], tf.multiply(tf.div(eta, batch_size),
+                                                                                              self.bias_gradient[0])))]
 
                     sess.run(steps, feed_dict={self.input: batch})
 
@@ -68,8 +65,8 @@ class Network:
 
     def calculate_cost(self, data, l2):
         l2_factor = l2 / (2 * data.shape[1])
-        output_l2 = tf.scalar_mul(l2_factor, tf.pow(tf.norm(self.output_weights), 2))
-        hidden_l2 = tf.scalar_mul(l2_factor, tf.pow(tf.norm(self.hidden_weights), 2))
+        output_l2 = tf.scalar_mul(l2_factor, tf.pow(tf.norm(self.weights[1]), 2))
+        hidden_l2 = tf.scalar_mul(l2_factor, tf.pow(tf.norm(self.weights[0]), 2))
 
-        return tf.add(tf.add(tf.div(self.loss_function.calculate_cost(data, self.output_activation), data.shape[1]),
+        return tf.add(tf.add(tf.div(self.loss_function.calculate_cost(data, self.activations[1]), data.shape[1]),
                              output_l2), hidden_l2)
